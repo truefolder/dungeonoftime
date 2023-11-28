6ea7b2a65faec08d34c83b45bddefacb47dac456
Тимур Бабаев <t.babaev@redkit-lab.ru>
2023-11-28 15:20:47 +0500

    Перенес все палитры и текстуры связанные с первым уровнем в отдельную папку

--- Assets/Tiled/Tilesets/Tiles-Props-pack.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tiles-Props-pack" tilewidth="32" tileheight="32" tilecount="99" columns="11">
- <image source="../Textures/Tiles-Props-pack.png" width="352" height="288"/>
-</tileset>
