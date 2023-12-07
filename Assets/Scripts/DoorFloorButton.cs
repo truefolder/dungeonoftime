d89a04f53465b8060375e1d9810b073676410461
Тимур Бабаев <t.babaev@redkit-lab.ru>
2023-12-07 16:04:34 +0500

    Второй уровень перенесен в игру

--- Assets/Scripts/DoorFloorButton.cs
+++ Assets/Scripts/DoorFloorButton.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
 
 public class DoorFloorButton : Door
 {
-    public FloorButton button;
+    public FloorButton[] buttons;
 
     private void Update()
     {
-        opened = button.isPressed;
+        opened = !buttons.Any(d => !d.isPressed);
         SetSprite();
         SetCollider();
     }
